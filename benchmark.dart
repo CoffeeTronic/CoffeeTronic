4e8f46f3918c36b793b6894ff1cc856add1c9f4b
Barbeitos <danielbarbeitos@gmail.com>
2014-03-18 12:43:49 +0100

    Archivo para crear las clases y funciones en Dart

---
+++ benchmark.dart
@@ -0,0 +1,13 @@
+/*
+ * Clases y funciones para el benchmark
+ * @author Daniel Barbeitos Carrillo 
+*/
+
+class Benchmark{
+  //Variables para controlar el tiempo antes y despues de las diferentes pruebas hardware del sistema.
+  int tAntes, tDespues;
+  
+  
+  
+  
+}
