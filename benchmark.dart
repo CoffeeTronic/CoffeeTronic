d7a78f5cc12d8bd402de24a2b129b6e6da64d47a
Barbeitos <danielbarbeitos@gmail.com>
2014-03-18 15:52:52 +0100

    Archivo Benchmarkt

--- benchmark.dart
+++ benchmark.dart
@@ -3,11 +3,44 @@
  * @author Daniel Barbeitos Carrillo 
 */
 
+import 'dart:core';
+import 'dart:math';
+
 class Benchmark{
   //Variables para controlar el tiempo antes y despues de las diferentes pruebas hardware del sistema.
-  int tAntes, tDespues;
+  DateTime tAntes, tDespues;
   
+  /*
+   * @brief Función que calcula la hora actual
+   * @return Retorna un DateTime con la hora actual cuando se ha creado el objeto
+   */
+  DateTime Time(){
+    DateTime ahora = new DateTime.now();
+    return ahora;
+  }
   
+  /*
+   * @brief Función que hará el benchmark al procesador
+   * @return Retorna un DateTime con el tiempo que ha tardado en ejecutar los comandos el procesador
+   */
+  DateTime Procesador(){
+    DateTime tTotal;
+    int ejecuciones = 1000000;
+    Random r = new Random();
+    
+    tAntes = Time();
+    
+    //Esta parte no sabia bien que poner
+    for(int i=0; i<ejecuciones; i++){
+      i*r.nextInt(ejecuciones);
+    }
+    
+    tDespues = Time();
+    
+    tTotal = tAntes.subtract(tDespues);
+    
+    return tTotal;
+  }
   
   
 }
