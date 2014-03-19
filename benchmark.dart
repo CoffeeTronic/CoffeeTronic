7c29ec1208642d1cb5ce6acafbeacaa76c9bfc6c
Barbeitos <danielbarbeitos@gmail.com>
2014-03-19 23:33:32 +0100

    Benchmark

--- benchmark.dart
+++ benchmark.dart
@@ -1,3 +1,7 @@
+/*void main() {
+  print("Hello, World!");
+}*/
+
 /*
  * Clases y funciones para el benchmark
  * @author Daniel Barbeitos Carrillo 
@@ -9,6 +13,13 @@ import 'dart:math';
 class Benchmark{
   //Variables para controlar el tiempo antes y despues de las diferentes pruebas hardware del sistema.
   DateTime tAntes, tDespues;
+  
+  /*
+   * @brief Contructor de la clase benchmark
+   */
+  Benchmark(){
+    
+  }
   
   /*
    * @brief Función que calcula la hora actual
@@ -26,7 +37,7 @@ class Benchmark{
      * @return Retorna un DateTime resultante de la recta de d1 y d2
   */
   DateTime SubstraerTiempos(DateTime d1, DateTime d2){
-    DateTime nuevo  = new DateTime((d1.year-d2.year), (d1.month-d2.month), (d1.day-d2.day), (d1.hour-d2.day), (d1.minute-d2.minute), (d1.second-d2.second), (d1.millisecond-d2.millisecond));
+    DateTime nuevo  = new DateTime((d2.year-d1.year), (d2.month-d1.month), (d2.day-d1.day), (d2.hour-d1.hour), (d2.minute-d1.minute), (d2.second-d1.second), (d2.millisecond-d1.millisecond));
     return nuevo;
   }
   
@@ -34,8 +45,8 @@ class Benchmark{
    * @brief Función que hará el benchmark al procesador
    * @return Retorna un DateTime con el tiempo que ha tardado en ejecutar los comandos el procesador
    */
-  DateTime Procesador(){
-    DateTime tTotal;
+  Duration Procesador(){
+    Duration tTotal;
     int ejecuciones = 1000000;
     Random r = new Random();
     
@@ -48,7 +59,8 @@ class Benchmark{
     
     tDespues = Time();
     
-    tTotal = SubstraerTiempos(tAntes, tDespues);
+    //tTotal = SubstraerTiempos(tAntes, tDespues);
+    tTotal = tDespues.difference(tAntes);
         
     return tTotal;
   }
@@ -114,5 +126,32 @@ class Benchmark{
     return tTotal;
   }
   
+  /*
+   * @brief Imprime el tiempo en cadena de texto
+   * @param tiempo Parametro que contiene el tiempo que se desea imprimir
+   * @return retorna una cadena de texto tipo String
+   */
+  String ImprimirResultado(DateTime tiempo){
+    print("prueba");
+    
+    print(tiempo);
+    
+    return tiempo.toString();
+  }
   
+}
+
+void main(){
+  Benchmark bench = new Benchmark();
+    
+  Duration tiempoPrueba = bench.Procesador();
+  
+  //print(tiempoPrueba.hour.toString()+":"+tiempoPrueba.minute.toString()+":"+tiempoPrueba.second.toString()+"."+tiempoPrueba.millisecond.toString());
+  
+  print(tiempoPrueba.toString());
+  
+  print("Microsegundos"+tiempoPrueba.inMicroseconds.toString());
+  print("Milisegundos"+tiempoPrueba.inMilliseconds.toString());
+  
+  //print(bench.ImprimirResultado(tiempoPrueba));
 }
